6bbcdfb9f9586681d8c5322281e4f99b2879095b
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-09-08 16:11:04 +0530

    init + setup

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="/icons/logo.png" type="image/x-icon" />
+                </Head>
+                <body className='bg-layout-100 sm:select-none'>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
