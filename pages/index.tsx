ecdcaa6595f1bc14c92a3509ecf9b1f471fe69b6
Trehan Sangpriya <trehansangpriya@gmail.com>
2022-12-08 12:53:04 +0530

    title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import { SocialIcons } from '@/components/app'
 import { Hero } from '@/sections'
 import { NextPage } from 'next'
+import Head from 'next/head'
 import React from 'react'
 
 const Home: NextPage = () => {
   return (
     <div className='px-[25px] md:px-[50px] lg:px-[100px] xl:px-[150px] 2xl:px-[200px]'>
+      <Head>
+        <title>Trehan Sangpriya</title>
+      </Head>
       {/* Fixed while scrolling */}
       <SocialIcons />
 
